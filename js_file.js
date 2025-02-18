bfb1e6f9ca6f4c3ffb95d5f7b4b639c91704bc5c
Nigel <ninmedium@gmail.com>
2025-02-19 02:31:32 +0800

    add operator function

--- js_file.js
+++ js_file.js
@@ -17,3 +17,18 @@ function divide(a, b) {
 let num1 = 0;
 let num2 = 0;
 let opr = "";
+
+function operate(num1, opr, num2) {
+    switch (opr) {
+        case "+":
+            return add(num1, num2);
+        case "-":
+            return subtract(num1, num2);
+        case "x":
+            return multiply(num1, num2);
+        case "/":
+            return divide(num1, num2);
+        default;
+            return "Error";
+    }
+}
